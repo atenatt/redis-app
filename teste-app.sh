cefada5a33b9f96998049639fc580a7a1209c4fd
Victor Pedro <56831201+atenatt@users.noreply.github.com>
2024-08-21 16:21:16 -0300

    ajustes no teste-app.sh

--- teste-app.sh
+++ teste-app.sh
@@ -3,7 +3,7 @@
 RESULT="`wget -qO- http://localhost:8090`"
 
 wget -q localhost:8090
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
     echo 'ok - servico no ar!'
 elif [[ $RESULT == *"Number"* ]]; then
     echo 'ok - number of visits'
